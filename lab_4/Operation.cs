57cfebfe186417748b63a14e90ffab44c308fae7
Тимур Ефименко <yefimenkotimur@gmail.com>
2022-11-18 17:00:21 +0200

    Добавьте файлы проекта.

---
+++ lab_4/Operation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab_4
+{
+    public class Operation
+    {
+        public static int GetPriority(string p0)
+        {
+            
+            switch (p0) 
+            {
+                case "+": return 1;
+                case "-": return 1;
+                case "*": return 2;
+                case "/": return 2;
+                case "%": return 2;
+                case "abs": return 3;
+                case "sqr": return 3;
+            }
+            return -1;
+        }
+        public static double CountOperator(string item, double a)
+        {
+            
+            switch (item) 
+            {
+                case "abs": return Math.Abs(a);
+                case "sqr": return Math.Pow(a, 2);
+                default: throw new Exception("Other operator. ");
+            }
+        } 
+        public static double CalculateOperations(string item, double a, double b)
+        {
+            switch (item) 
+            {
+                case "+": return a + b;
+                case "-": return a - b;
+                case "*": return a * b;
+                case "/": return a / b;
+                case "%": return a % b;
+                default: return 0;
+            }
+        }
+        public static bool IsOperator(string item)
+        {
+            string[] operators = { "+", "-", "*", "/", "%" };
+            return operators.Contains(item);
+        }
+    }
+}
---
+++ lab_4/Symbol.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab_4
+{
+    public class Symbol
+    {
+        public static bool IsLetter(char letter)
+        {
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            return letters.Contains(letter);
+        }
+
+        public static bool IsOperator(string op)
+        {
+            string[] operators = { "+", "-", "*", "/", "%", "sqr", "abs" };
+            return operators.Contains(op);
+        }
+        
+        public static bool IsNumber(string input)
+        {
+            string numbers = "0123456789";
+            while (input.Length > 0)
+            {
+                if (!numbers.Contains(input[0]))
+                {
+                    return false;
+                }
+                input = input.Substring(1);
+            }
+            return true;
+        } 
+
+        public static bool IsDigit(char digit)
+        {
+            string numbers = "0123456789";
+            return numbers.Contains(digit);
+        }
+    }
+}
